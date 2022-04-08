33174c7691e81c3dd943f5cd47c66203dedcc875
Kenan <kenan.genjac2019@size.ba>
2022-04-09 01:16:07 +0200

    user CRUD finished

--- modules/users/userController.js
+++ modules/users/userController.js
@@ -70,3 +70,57 @@ export const signIn = async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+export const changeUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User doesn't exist." });
+    }
+    if (user._id == req.params.id) {
+      if (req.body.password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      }
+      try {
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.id,
+          { $set: req.body },
+          { new: true }
+        );
+        res.status(200).json(updatedUser);
+      } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+      }
+    } else {
+      return res
+        .status(401)
+        .json({ message: "You can only update Your account" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User doesn't exist." });
+    }
+    if (user._id == req.params.id) {
+      try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "User deleted" });
+      } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+      }
+    } else {
+      return res
+        .status(401)
+        .json({ message: "You can delete only your account" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
